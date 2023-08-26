aeedc04e94a89baa10b6a9c0f61eb7c517414735
NimishaBondugula <bondugulanimisha@gmail.com>
2023-08-26 21:18:52 +0530

    Change Fully To Tel

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ function goToSamhitha() {
     button2.classList.remove("clicked"); 
     var button = document.getElementById("showTel"); 
     button.classList.add("clicked"); 
+    //showing telugu cards
     var hiddenElement = document.getElementById("telElement"); 
     hiddenElement.style.display = "block"; 
+    //hiding english cards
     var hideElement = document.getElementById("engElement"); 
     hideElement.style.display = "none";
+    //showing telugu title
+    var hiddenElement = document.getElementById("telHeading"); 
+    hiddenElement.style.display = "block"; 
+    //hiding english title
+    var hideElement = document.getElementById("engHeading"); 
+    hideElement.style.display = "none";
+     //showing telugu subtitle
+     var hiddenElement = document.getElementById("telSubtitle"); 
+     hiddenElement.style.display = "block"; 
+     //hiding english subtitle
+     var hideElement = document.getElementById("engSubtitle"); 
+     hideElement.style.display = "none";
 
  }); 
 
@@ -19,8 +33,23 @@ function goToSamhitha() {
     button2.classList.remove("clicked"); 
     var button = document.getElementById("showEng"); 
     button.classList.add("clicked"); 
+    //showing english cards
     var hiddenElement = document.getElementById("engElement"); 
     hiddenElement.style.display = "block"; 
+    //hiding telugu cards
     var hideElement = document.getElementById("telElement"); 
     hideElement.style.display = "none";
+     //showing english title
+     var hiddenElement = document.getElementById("engHeading"); 
+     hiddenElement.style.display = "block"; 
+     //hiding telugu title
+     var hideElement = document.getElementById("telHeading"); 
+     hideElement.style.display = "none";
+     //showing english subtitle
+     var hiddenElement = document.getElementById("engSubtitle"); 
+     hiddenElement.style.display = "block"; 
+     //hiding telugu subtitle
+     var hideElement = document.getElementById("telSubtitle"); 
+     hideElement.style.display = "none";
+
  }); 
